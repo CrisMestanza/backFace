619066f2a56928fe44dadedc0c22721c9d3d34b6
Cristian Mestanza Ortiz <cristianmestanzaortiz870@gmail.com>
2026-03-03 21:37:47 -0500

    Subir a vps

--- puebajeje.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-URL = "https://fbadb501d25b.ngrok-free.app/recognize"  # <- tu URL pública
-files = {"image": ("frame.jpg", open("cara.jpg","rb"), "image/jpeg")}
-r = requests.post(URL, files=files, timeout=10)
-print(r.json())
